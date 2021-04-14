ba11a5273432e7e4ddf18b4ceb8ad8909b87a892
Aaron <aaron@sajatypeworks.com>
2021-04-14 10:02:26 -0700

    Updating font name
    Removing "Font" from fontname

--- build.py
+++ build.py
@@ -8,19 +8,19 @@ import ufo2ft
 import os
 
 print ("Converting to UFO")
-main(("glyphs2ufo", "sources/YomogiFont.glyphs"))
+main(("glyphs2ufo", "sources/Yomogi.glyphs"))
 
-exportFont = ufoLib2.Font.open("sources/YomogiFont-Regular.ufo")
+exportFont = ufoLib2.Font.open("sources/Yomogi-Regular.ufo")
 
 exportFont.lib['com.github.googlei18n.ufo2ft.filters'] = [{
     "name": "flattenComponents",
     "pre": 1,
 }]
 
-print ("[YomogiFont] Compiling")
+print ("[Yomogi] Compiling")
 static_ttf = ufo2ft.compileTTF(exportFont, removeOverlaps=True)
 
-print ("[YomogiFont] Adding bits")
+print ("[Yomogi] Adding bits")
 static_ttf["DSIG"] = newTable("DSIG")
 static_ttf["DSIG"].ulVersion = 1
 static_ttf["DSIG"].usFlag = 0
@@ -28,19 +28,19 @@ static_ttf["DSIG"].usNumSigs = 0
 static_ttf["DSIG"].signatureRecords = []
 static_ttf["head"].flags |= 1 << 3        #sets flag to always round PPEM to integer
 
-print ("[YomogiFont] Saving")
-static_ttf.save("fonts/ttf/YomogiFont-Regular.ttf")
+print ("[Yomogi] Saving")
+static_ttf.save("fonts/ttf/Yomogi-Regular.ttf")
 
-shutil.rmtree("sources/YomogiFont-Regular.ufo")
-os.remove("sources/YomogiFont.designspace")
+shutil.rmtree("sources/Yomogi-Regular.ufo")
+os.remove("sources/Yomogi.designspace")
 
 subprocess.check_call(
         [
             "ttfautohint",
             "--stem-width",
             "nsn",
-            "fonts/ttf/YomogiFont-Regular.ttf",
-            "fonts/ttf/YomogiFont-Regular-hinted.ttf",
+            "fonts/ttf/Yomogi-Regular.ttf",
+            "fonts/ttf/Yomogi-Regular-hinted.ttf",
         ]
     )
-shutil.move("fonts/ttf/YomogiFont-Regular-hinted.ttf", "fonts/ttf/YomogiFont-Regular.ttf")
+shutil.move("fonts/ttf/Yomogi-Regular-hinted.ttf", "fonts/ttf/Yomogi-Regular.ttf")
